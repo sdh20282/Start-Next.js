8332a5a33a79febed5b3b0a6e293107754775813
sdh20282 <sdh20282@gmail.com>
2024-03-29 18:27:58 +0900

    Feat : implement Move Detail Page styles and features

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -10,13 +10,9 @@ export default async function MovieDetail({
 
   return (
     <div>
-      <h3>Movie Detail Page</h3>
-
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-
-      <h4>Videos</h4>
 
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
